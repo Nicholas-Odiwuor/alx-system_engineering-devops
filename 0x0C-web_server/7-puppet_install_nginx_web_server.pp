9d26d1707dcbf1bfdc4fa5b391c79306fb735f1e
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-04-23 06:06:24 -0700

    Installation

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,48 @@
-# This manifest is used to configure nginx web server during installation
+#!/usr/bin/env bash
+# Puppet manifest to install and configure Nginx web server
+
+# Install Nginx package
 package { 'nginx':
-    ensure => 'installed',
+  ensure => installed,
 }
 
-exec {'install':
-    command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-    provider => shell,
-}
+# Ensure Nginx service is enabled and running
 service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
 
-file {'/etc/nginx/sites-available/default':
-    ensure  => 'file',
-    content => '
-       server {
-           listen 80;
-           listen [::]:80 default_server;
-           root /etc/nginx/html;
-           index.html index.htm;
-       
-            location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-            }
-        }
-    ',
-    require => Package['nginx'],
-    notify  => Service['nginx']
-}
-file {'/etc/nginx/html/index.html':
-    ensure  => 'file',
-    content => 'Hello World',
+# Configure Nginx to listen on port 80 and serve "Hello World!" at the root
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    server_name _;
+
+    location / {
+        return 301 https://$host$request_uri;
+    }
+
+    location /redirect_me {
+        return 301 https://www.example.com/;
+    }
 }
 
-exec {'run':
-    command  => 'sudo service nginx restart',
-    provider => shell,
+',
+  notify  => Service['nginx'],
 }
+
+# Create HTML file with "Hello World!" content
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  notify  => Service['nginx'],
+}
+

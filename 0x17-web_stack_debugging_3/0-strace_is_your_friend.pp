cde8d398b12ddbaf1ce11d41a333f3d86ce58a3c
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-02-26 12:04:14 -0800

    Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes error 500 in Apache web server.
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}

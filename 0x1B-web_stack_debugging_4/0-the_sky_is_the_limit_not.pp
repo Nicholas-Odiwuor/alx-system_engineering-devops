2e073eec377678cef8db5792e833665dc8c49616
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-03-05 23:13:39 -0800

    0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,19 @@
-#!/usr/bin/puppet
-# Puppet manifest to optimize Nginx and reduce failed requests
+# Fix the problem of high amount of requests.
 
-class optimize_nginx {
-    exec { 'increase-nginx-limit':
-        command => '/bin/echo "worker_rlimit_nofile=65535;" >> /etc/nginx/nginx.conf && sed -i "/http {/a \ \ \ \ worker_connections 65535;" /etc/nginx/nginx.conf && service nginx restart',
-        path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-        onlyif  => '/bin/grep -q -E "worker_connections [0-9]+;" /etc/nginx/nginx.conf',
-    }
+exec {'replace':
+
+  provider => shell,
+
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+
+  before   => Exec['restart'],
+
 }
 
-include optimize_nginx
+exec {'restart':
 
+  provider => shell,
+
+  command  => 'sudo service nginx restart',
+
+}
